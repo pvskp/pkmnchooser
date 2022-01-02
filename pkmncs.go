3bfd7b295a5cd897153c7160a399f780c279e52c
pvskp <pvincius14@gmail.com>
2022-01-02 19:05:47 -0300

    Implementa nome dos pokemons em uma lista

--- pkmncs.go
+++ pkmncs.go
@@ -6,14 +6,33 @@ import (
     "fmt"
     "encoding/json"
     "os"
+    "sort"
 )
 
-// getMoveInfo returns a slice of maps (pokemons) that can learn
-// the move specified in the parameters.
-func getMoveInfo (  ) {
-    //TODO: Parse via "learned by"
-    //TODO: Parse via "learn via TM/HM"
-    //TODO: Parse via "egg move"
+func getMoveGet (  ){
+    panic ( "getMoveGet not implemented yet" )
+}
+
+// getMoveLearnedBy receives a byte response from a API call and convert it and returns a map. 
+func getMoveLearnedBy ( responseData []byte ) ( pkmnList []string ) {
+    pkmnMap := make ( map[string]( []map[string]string ) )
+    json.Unmarshal ( responseData, &pkmnMap )
+    learnedBy := pkmnMap["learned_by_pokemon"]
+    
+    // var pkmnList []string
+
+    for key := range learnedBy {
+        pkmnList = append(pkmnList, learnedBy[key]["name"])
+    }
+
+    sort.Strings ( pkmnList )
+
+    fmt.Println ( pkmnList )
+
+    return pkmnList
+}
+
+func getMoveInfo () {
     moveData := "https://pokeapi.co/api/v2/move/" + os.Args[1]
 
     moveByte, callError  :=  http.Get ( moveData )
@@ -29,13 +48,9 @@ func getMoveInfo (  ) {
         panic ( "Error while reading byte body" )
     }
 
-    pkmnMap := make ( map[string]( []map[string]string ) )
-    json.Unmarshal ( responseData, &pkmnMap )
-    learnedBy := pkmnMap["learned_by_pokemon"]
-    
-    for key := range learnedBy { // list all pokémons that can learn the move
-        fmt.Printf( "%s ", learnedBy[key]["name"] )
-    }
+     getMoveLearnedBy ( responseData )
+
+
 }
  
 func main (){
