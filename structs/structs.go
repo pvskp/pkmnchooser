e3b71212f8f02cc337e8f2ac1fe6e914df9f252d
pvskp <pvincius14@gmail.com>
2022-05-07 01:45:44 -0300

    Backup

--- structs/structs.go
+++ structs/structs.go
@@ -1,27 +1,24 @@
 package structs
 
 type LearnedBy struct {
-	Pokemons []struct {
-		Name string `json:"name"`
-	} `json:"learned_by_pokemon"`
+    Pokemons []struct {
+        Name string `json:"name"`
+    } `json:"learned_by_pokemon"`
 }
 
 type Pokemon struct {
-	Moves []struct {
-		Move struct {
-			Name string `json:"name"`
-		} `json:"move"`
+    Moves []struct {
+        Move struct {
+            Name string `json:"name"`
+        } `json:"move"`
 
-		VersionDetails []struct {
-			LevelLearnedAt int `json:"level_learned_at"`
+        VersionDetails []struct {
+            LevelLearnedAt int `json:"level_learned_at"`
 
-			LearnMethod struct {
-				Name string `json:"name"`
-			} `json:"move_learn_method"`
-
-			VersionGroup struct {
-				Name string `json:"name"`
-			} `json:"version_group"`
-		} `json:"version_group_details"`
-	} `json:"moves"`
+            // LearnMethod struct []
+            VersionGroup struct {
+                Name string `json:"name"`
+            } `json:"version_group"`
+        } `json:"version_group_details"`
+    } `json:"moves"`
 }
